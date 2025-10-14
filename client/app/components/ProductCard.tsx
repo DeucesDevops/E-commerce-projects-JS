f5942f79d846be76eb893f9fe44a434a257ac718
DeucesDevops <132962088+DeucesDevops@users.noreply.github.com>
2025-10-14 18:48:51 +0100

    Display Featured icon and  Category component

---
+++ client/app/components/ProductCard.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import React from 'react'
+import {ProductType} from "@/app/type";
+
+const ProductCard = ({product}:{product:ProductType}) => {
+    return (
+        <div>ProductCard</div>
+    )
+}
+export default ProductCard
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import Image from "next/image";
+import ProductList from "@/app/components/ProductList";
 
 const HomePaage = () => {
     return (
-        <div className="">HomePaage</div>
+        <div className="">
+            <div className="relative aspect-[3/1] mb-12">
+                <Image src="/featured.png" alt="Featured Product" fill/>
+
+            </div>
+            <ProductList />
+        </div>
     )
 }
 export default HomePaage
